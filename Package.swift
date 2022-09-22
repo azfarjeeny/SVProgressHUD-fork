ff1a00037eb325d4925a0093669aaada975901a9
Azfar Hussain <azfar.hussain@jeeny.me>
2022-09-22 15:44:13 +0500

    renamed Assets

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .target(
             name: "SVProgressHUD",
             path: "Sources/SVProgressHUD",
-            resources: [.copy("Resources")],
+            resources: [.copy("Assets")],
             publicHeadersPath: "Headers"
         )
     ]
