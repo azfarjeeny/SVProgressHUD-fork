0b729aa6e2288287b3b975703d096000fdb0dede
Azfar Hussain <azfar.hussain@jeeny.me>
2022-09-22 15:33:06 +0500

    forked code

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "SVProgressHUD",
+    platforms: [
+        .iOS("9.0")
+    ],
+    products: [
+        .library(name: "SVProgressHUD", targets: ["SVProgressHUD"]),
+    ],
+    targets: [
+        .target(
+            name: "SVProgressHUD",
+            path: "Sources/SVProgressHUD",
+            resources: [.copy("Resources")],
+            publicHeadersPath: "Headers"
+        )
+    ]
+)
