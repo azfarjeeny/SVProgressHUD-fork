e31bca3b9658ad242ef14583ccdf843d0d3d3921
Azfar Hussain <azfar.hussain@jeeny.me>
2022-09-22 17:13:49 +0500

    changes

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .target(
             name: "SVProgressHUD",
             path: "Sources/SVProgressHUD",
-            resources: [.process("Assets")],
+            resources: [.copy("Assets")],
             publicHeadersPath: "Headers"
         )
     ]
